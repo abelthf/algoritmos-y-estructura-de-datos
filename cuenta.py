62d6782b1f93a5b09c9b059d0c6ac6363a46d4ba
Fredy Abel Huanca Torres <abel.huanca@upeu.pe>
2013-09-26 17:12:58 -0500

    cuenta negativos

--- cuenta.py
+++ cuenta.py
@@ -5,7 +5,23 @@ contar cuanto positivos, negativos, pares, impares existen
 """
 # -*- coding: utf-8 -*-
 
+def esNegativo(num):
+    if n < 0:
+        return True
+    else:
+        return False
+
+cn = 0
 while True:
     n=input("ingrese un numero")
+    if esNegativo(n):
+        cn=cn+1
+    elif
+
+
+
+
     if not n!=0:
         break
+
+print "negativos = %s" % cn
