f2a34dbcbbfeeb6abb40c5d323cf845796479b2e
Fredy Abel Huanca Torres <abel.huanca@upeu.pe>
2013-09-26 15:14:05 -0500

    pi por do while

---
+++ pi.py
@@ -0,0 +1,15 @@
+import os
+os.system('clear')
+
+i = 1
+pi4 = 0
+signo = -1
+
+while True:
+    signo = -signo
+    pi4 = pi4 + signo*(1.0/i)
+    i = i+2
+    if not 1.0/i > 0.000000001:
+        break
+
+print "pi vale %s" % (pi4*4)
