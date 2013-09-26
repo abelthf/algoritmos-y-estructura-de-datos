0d652a5abf7d542f1aa6d8d9513b9e11f12e9a1f
Fredy Abel Huanca Torres <abel.huanca@upeu.pe>
2013-09-26 17:01:37 -0500

    cuenta pares, impares, ....

---
+++ cuenta.py
@@ -0,0 +1,11 @@
+"""
+Programa que emula Do While
+ingresa una serie de numeros, y cuando es 0 termina, para luego
+contar cuanto positivos, negativos, pares, impares existen
+"""
+# -*- coding: utf-8 -*-
+
+while True:
+    n=input("ingrese un numero")
+    if not n!=0:
+        break
