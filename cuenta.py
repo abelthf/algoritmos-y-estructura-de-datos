94ae43b23ff69fcd2b9f83cb5bed949fe04d6204
Fredy Abel Huanca Torres <abel.huanca@upeu.pe>
2013-09-26 17:28:25 -0500

    final contador de pares, impares, positivos, negativos

--- cuenta.py
+++ cuenta.py
@@ -11,17 +11,46 @@ def esNegativo(num):
     else:
         return False
 
+def esPositivo(num):
+    if n > 0:
+        return True
+    else:
+        return False
+
+
+def esPar(num):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def esImpar(num):
+    if n % 2 != 0:
+        return True
+    else:
+        return False
+
 cn = 0
+cp = 0
+cpares = 0
+cimpares = 0
 while True:
     n=input("ingrese un numero")
     if esNegativo(n):
-        cn=cn+1
-    elif
+        cn = cn + 1
+    if esPositivo(n):
+        cp = cp + 1
 
+    if esPar(n):
+        cpares = cpares + 1
 
-
-
+    if esImpar(n):
+        cimpares = cimpares + 1
     if not n!=0:
         break
 
 print "negativos = %s" % cn
+print "positivos = %s" % cp
+print "pares = %s" % cpares
+print "impares = %s" % cimpares
